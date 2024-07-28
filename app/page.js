68cc8502cee70d8f3d854b6ad6b5b9761328e520
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-07-28 19:56:25 +0100

    anything  ut jack

--- app/page.js
+++ app/page.js
@@ -1,45 +1,47 @@
 "use client";
-
 import { useState } from "react";
-
 import { FormControl, FormErrorMessage, FormLabel, Heading, Input, Textarea } from "@chakra-ui/react";
-
 const initValues = { name: "", subject: "", message: "", email: "" };
-
 const initState = { values: initValues };
-
 export default function Home() {
   const [state, setState] = useState(initState);
-
   const { values } = state;
-
-  const handleChange = ({target}) => setState((prev) => ({
+  const handleChange = ({ target }) => setState((prev) => ({
     ...prev,
     values: {
       ...prev.values,
-      [target.name]: target.values
+      [target.name]: target.value
     }
   }));
+
+
+  
   return (
-    <div className="p-10  w-full flex items-center justify-center flex-col">
+    <div className="p-10 w-full flex items-center justify-center flex-col">
       <Heading className="font-poppins py-10">Contact</Heading>
 
       <FormControl isRequired isInvalid={!values.name} mb={5}>
         <FormLabel>Name</FormLabel>
-        <Input type="text" name="Name" onChange={handleChange} errorBorderColor="red.300" value={values.name} />
+        <Input type="text" name="name" onChange={handleChange} errorBorderColor="red.300" value={values.name} />
         <FormErrorMessage>Required</FormErrorMessage>
       </FormControl>
-      <FormControl isRequired mb={5}>
+
+      <FormControl isRequired isInvalid={!values.email} mb={5}>
         <FormLabel>Email</FormLabel>
-        <Input type="text" name="email" onChange={handleChange} />
+        <Input type="text" name="email" onChange={handleChange} errorBorderColor="red.300" value={values.email} />
+        <FormErrorMessage>Required</FormErrorMessage>
       </FormControl>
-      <FormControl isRequired mb={5}>
+
+      <FormControl isRequired isInvalid={!values.subject} mb={5}>
         <FormLabel>Subject</FormLabel>
-        <Input type="text" name="subject" onChange={handleChange} />
+        <Input type="text" name="subject" onChange={handleChange} errorBorderColor="red.300" value={values.subject} />
+        <FormErrorMessage>Required</FormErrorMessage>
       </FormControl>
-      <FormControl isRequired mb={5}>
+
+      <FormControl isRequired isInvalid={!values.message} mb={5}>
         <FormLabel>Message</FormLabel>
-        <Textarea type="text" name="message" rows="4" onChange={handleChange} />
+        <Textarea name="message" onChange={handleChange} errorBorderColor="red.300" value={values.message} rows="4" />
+        <FormErrorMessage>Required</FormErrorMessage>
       </FormControl>
     </div>
   );
